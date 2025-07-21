e2d93616ae58bbc3e55a5dad147061396bc61197
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-07-21 15:11:42 -0300

    About Page Improvment

--- app/controllers/about_pages_controller.ts
+++ app/controllers/about_pages_controller.ts
@@ -3,14 +3,11 @@ import AboutPage from '#models/about_page'
 import vine from '@vinejs/vine'
 
 export default class AboutPagesController {
-
-  // Apenas busca o registro e o retorna como está
   async show({ response }: HttpContext) {
     const record = await AboutPage.firstOrCreate({}, {})
     return response.ok(record)
   }
 
-  // Recebe e salva o 'content' como um texto simples
   async update({ request, response }: HttpContext) {
     const schema = vine.object({
       content: vine.string().nullable(),
@@ -20,17 +17,13 @@ export default class AboutPagesController {
     try {
       const payload = await vine.validate({ schema, data: request.all() })
       const record = await AboutPage.firstOrFail()
-
-      // Converte 'null' em uma string vazia para ser compatível com o Model
       const contentToSave = payload.content ?? ''
       const informationsToSave = payload.informations ?? ''
-
       record.merge({
         content: contentToSave,
         informations: informationsToSave,
       })
       await record.save()
-
       return response.ok({ message: 'Página Sobre atualizada com sucesso!' })
     } catch (error) {
       return response.badRequest({ message: 'Erro ao salvar os dados.', errors: error.messages })
